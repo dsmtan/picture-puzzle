c2e1f38232a9859eb9ab7785acd8ac440462b53e
dsmtan <denisetan@live.nl>
2019-02-18 23:06:17 +0100

    added working puzzle

---
+++ denise.js
@@ -0,0 +1,128 @@
+"use strict";
+
+const puzzleBtn = document.querySelector("#puzzleBtn");
+const puzzleImage = document.querySelector("img");
+const container = document.querySelector("#container");
+const looseDiv = document.querySelector("#looseDiv");
+
+document.addEventListener("DOMContentLoaded", startTheScript);
+
+//load image when button is clicked
+function startTheScript() {
+  puzzleBtn.addEventListener("click", loadTheImage);
+}
+
+function loadTheImage() {
+  puzzleImage.src = document.querySelector("#imageurl").value;
+  puzzleImage.onload = theImageHasLoaded;
+}
+
+function theImageHasLoaded() {
+  //   alert("THE IMAGE HAS LOADED");
+  createGrid();
+}
+
+//display puzzle grid
+
+function createGrid() {
+  // user input x and y number of pieces
+  const numOfXPieces = document.querySelector("#xPieces").value;
+  const numOfYPieces = document.querySelector("#yPieces").value;
+
+  // grid container size adjusts to image
+  const container_width = puzzleImage.clientWidth;
+  const container_height = puzzleImage.clientHeight;
+
+  //create grid template
+  container.style.gridTemplateColumns = `repeat(${numOfXPieces}, 1fr)`;
+  container.style.width = `${container_width}px`;
+
+  looseDiv.style.gridTemplateColumns = `repeat(${numOfXPieces}, 1fr)`;
+  looseDiv.style.width = `${container_width}px`;
+
+  //create div for each x/y input number
+  for (let y = 0; y < numOfYPieces; y++) {
+    for (let x = 0; x < numOfXPieces; x++) {
+      //create grid - dropzone
+      let gridPiece = document.createElement("div");
+
+      gridPiece.style.height = container_height / numOfYPieces + "px";
+      gridPiece.id = `grid${x}${y}`;
+
+      gridPiece.classList.add("dropzone");
+      container.appendChild(gridPiece);
+    }
+  }
+
+  //for each gridPiece that is created - create a puzzle Piece of same size
+  for (let y = 0; y < numOfYPieces; y++) {
+    for (let x = 0; x < numOfXPieces; x++) {
+      //create loose puzzle pieces
+      let puzzlePiece = document.createElement("div");
+
+      let pieceHeight = container_height / numOfYPieces + "px";
+      let pieceWidth = container_width / numOfXPieces + "px";
+
+      puzzlePiece.style.height = pieceHeight;
+      puzzlePiece.style.width = pieceWidth;
+      puzzlePiece.id = `puzzle${x}${y}`;
+      puzzlePiece.classList.add("draggable");
+
+      let imageURL = document.querySelector("#imageurl").value;
+      puzzlePiece.style.backgroundImage = `url("${imageURL}")`;
+      puzzlePiece.style.backgroundSize = `${container_width}px ${container_height}px`;
+      puzzlePiece.style.backgroundRepeat = "no-repeat";
+      puzzlePiece.style.backgroundPositionX = `calc(-${pieceWidth}*${x})`;
+      puzzlePiece.style.backgroundPositionY = `calc(-${pieceHeight}*${y})`;
+      puzzlePiece.style.top = `calc(${Math.random() * 500}px + 15vh)`;
+      puzzlePiece.style.left = `calc(${Math.random() * 400}px + 45vw)`;
+
+      looseDiv.appendChild(puzzlePiece);
+    }
+  }
+}
+
+//drag & drop puzzle pieces
+
+let dragged;
+
+/* events fired on the draggable target */
+document.addEventListener("drag", function(event) {});
+
+document.addEventListener("dragstart", function(event) {
+  // store a ref. on the dragged elem
+  dragged = event.target;
+  // make it half transparent
+  dragged.style.opacity = 0.5;
+});
+
+document.addEventListener("dragend", function(event) {
+  // reset the transparency
+  event.target.style.opacity = "";
+});
+
+/* events fired on the drop targets */
+document.addEventListener("dragover", function(event) {
+  // prevent default to allow drop
+  event.preventDefault();
+});
+
+document.addEventListener("drop", function(event) {
+  // prevent default action (open as link for some elements)
+  event.preventDefault();
+  console.log("DROP", event.target.className);
+  // move dragged elem to the selected drop target
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "";
+
+    dragged.parentNode.removeChild(dragged);
+    event.target.appendChild(dragged);
+
+    dragged.style.left = event.target.style.left;
+    dragged.style.top = event.target.style.top;
+  } else if (event.target.className == "theBody") {
+    // park the dragged elem somewhere on the body
+    dragged.style.left = event.pageX + "px";
+    dragged.style.top = event.pageY + "px";
+  }
+});
